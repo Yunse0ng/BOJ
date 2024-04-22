e2494db99773ee1300d372603c7ed473c443b3eb
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 00:45:22 +0900

    solved the 1008

---
+++ src/boj_1008/Kotlin.kt
@@ -0,0 +1,7 @@
+package boj_1008
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    println(nextDouble() / nextDouble())
+}
---
+++ src/boj_1008/Main.java
@@ -0,0 +1,11 @@
+package boj_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextDouble() / scanner.nextDouble());
+    }
+}
