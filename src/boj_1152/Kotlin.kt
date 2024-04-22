2b19213dbb1bf0ecd2858ecf5e420aae1a0b451d
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 00:01:43 +0900

    solved the 1152

---
+++ src/boj_1152/Kotlin.kt
@@ -0,0 +1,10 @@
+package boj_1152
+
+import java.util.*
+
+fun main(){
+    val s: String = readln().trim()
+    val stringTokenizer = StringTokenizer(s)
+
+    println(stringTokenizer.countTokens())
+}
