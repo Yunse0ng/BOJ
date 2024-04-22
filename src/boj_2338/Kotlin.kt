1a2f2fa7622646e6d6294cf351fa8e1c6fb453ed
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 01:25:30 +0900

    solved the 2338

---
+++ src/boj_2338/Kotlin.kt
@@ -0,0 +1,12 @@
+package boj_2338
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextBigInteger()
+    val b = nextBigInteger()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
