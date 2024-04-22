aad2c0cd1d1e5f07b5bf6351dd020e7c29b0c304
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 00:18:43 +0900

    solved the 1000

---
+++ src/boj_1000/Main.java
@@ -0,0 +1,11 @@
+package boj_1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
