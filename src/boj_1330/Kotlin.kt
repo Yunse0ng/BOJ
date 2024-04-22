ff048c6437dfb8fa428542a16d2b4e700cbecf05
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 02:18:45 +0900

    solved the 1330

---
+++ src/boj_1330/Kotlin.kt
@@ -0,0 +1,12 @@
+package boj_1330
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+
+    if (a > b) println(">")
+    else if (a < b) println("<")
+    else println("==")
+}
