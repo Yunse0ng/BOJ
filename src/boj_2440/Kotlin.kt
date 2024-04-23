a64b3e1209d182c450b2ef275a0611b9f2c279f7
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 09:53:18 +0900

    solved the 2440

--- src/boj_2440/Kotlin.kt
+++ src/boj_2440/Kotlin.kt
@@ -3,8 +3,8 @@ package boj_2440
 fun main() {
     val a = readln().toInt()
 
-    for (i in a downTo 1) { // 5 4 3 2 1
-        for (j in i downTo 1)  // 5 4 3 2 1
+    for (i in a downTo 1) {
+        for (j in i downTo 1)
             print("*")
         println()
     }
