851bbd8f0f107643622ad0766ed44d4c732ac0dd
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 21:51:40 +0900

    solved the 27889

---
+++ src/boj_27889/Kotlin.kt
@@ -0,0 +1,13 @@
+package boj_27889
+
+fun main() {
+    val a: String = readln()
+    if (a == "NLCS")
+        println("North London Collegiate School")
+    else if (a == "BHA")
+        println("Branksome Hall Asia")
+    else if (a == "KIS")
+        println("Korea International School")
+    else
+        println("St. Johnsbury Academy")
+}
