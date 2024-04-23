f60ae45e31433b50ebb3261aaf02f22a2c32c244
Yunseong Jeong <jyerd333@gmail.com>
2024-04-24 08:36:52 +0900

    solved the 28235

---
+++ src/boj_28235/Kotlin.kt
@@ -0,0 +1,13 @@
+package boj_28235
+
+fun main() {
+    val a = readln()
+    if (a == "SONGDO")
+        println("HIGHSCHOOL")
+    else if (a == "CODE")
+        println("MASTER")
+    else if (a == "2023")
+        println("0611")
+    else if (a == "ALGORITHM")
+        println("CONTEST")
+}
