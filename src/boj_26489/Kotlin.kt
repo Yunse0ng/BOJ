bc6d165ece9241cb4c143618f349c3e0b88d2314
Yunseong Jeong <jyerd333@gmail.com>
2024-04-23 19:45:58 +0900

    solved the 26489

---
+++ src/boj_26489/Kotlin.kt
@@ -0,0 +1,13 @@
+package boj_26489
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var cnt = 0
+    while (true) {
+        br.readLine() ?: break
+        cnt++
+    }
+    println(cnt)
+}
