80313e2f8acaf8baaaeee6c121462348a540f8d4
Yunseong Jeong <jyerd333@gmail.com>
2024-06-20 16:08:18 +0900

    solved the 23696

---
+++ src/boj_23969/Main.java
@@ -0,0 +1,45 @@
+package boj_23969;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int count = 0;
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for(int i = 0; i < a; i++) {
+            arr.add(scanner.nextInt());
+        }
+
+        for(int i = 1; i < arr.size(); i++) {
+            for(int j = 0; j < arr.size() - i; j++) {
+                if(arr.get(j) > arr.get(j + 1)){
+                    if(count == b){
+                        break;
+                    }
+                    swap(arr, j, j+1);
+                    count++;
+                }
+            }
+        }
+        if(count < b) System.out.println("-1");
+        else{
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void swap(ArrayList<Integer> arr, int first, int second) {
+        int temp;
+        temp = arr.get(first);
+        arr.set(first, arr.get(second));
+        arr.set(second, temp);
+    }
+}
